358e47e551aa2e2fda7165c123ea43c762c8b52d
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2019-12-09 17:02:33 +0700

    Inheritance

---
+++ week_2/src/JavaInheritance/SuperClass.java
@@ -0,0 +1,17 @@
+package JavaInheritance;
+
+public class SuperClass {
+    int num = 20;
+
+    // display method of superclass
+    public void display() {
+        System.out.println("This is the display method of superclass");
+    }
+
+    public SuperClass(int num) {
+        this.num = num;
+    }
+
+    public SuperClass() {
+    }
+}
