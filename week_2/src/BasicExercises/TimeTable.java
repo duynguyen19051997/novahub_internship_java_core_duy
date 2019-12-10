1284302083c36dd346e0af5e725cf0629edf44d4
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2019-12-10 15:23:19 +0700

    Exercises

--- week_2/src/BasicExercises/TimeTable.java
+++ week_2/src/BasicExercises/TimeTable.java
@@ -2,20 +2,21 @@ package BasicExercises;
 
 import java.util.Scanner;
 
-public class CheckerPattern {
+public class TimeTable {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.print("Enter your size: ");
         int size = Integer.parseInt(SCANNER.nextLine());
+        System.out.print("*\t|\t");
         for (int i = 1; i <= size; i++) {
-            int j = 0, row = size - 1;
-            if (i % 2 == 1) {
-                j = 1; row = size;
-                System.out.print(" ");
-            }
-            for (; j <= row; j++) {
-                System.out.print("* ");
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+        for (int i = 1; i <= size; i++) {
+            System.out.print(i + "\t|\t");
+            for (int j = i; j <= size * i; j += i) {
+                System.out.print(j + "\t");
             }
             System.out.println();
         }
